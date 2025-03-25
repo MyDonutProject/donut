cd88fa06e4c28340832b56677ccc4db3fc2c5087
MyDonutProject <GhostNinjaX@proton.me>
2025-03-25 19:15:11 -0300

    adjusts

--- src/constants/translateCountries.ts
+++ src/constants/translateCountries.ts
@@ -1,34 +1,34 @@
-import { LanguageListProps } from '@/types/translate/props';
+import { LanguageListProps } from "@/types/translate/props";
 
 export const languageList: LanguageListProps[] = [
   {
-    id: 1n,
-    name: 'Portuguese',
-    lang: 'pt',
-    image: 'br.svg',
-  },
-  {
     id: 2n,
-    name: 'English',
-    lang: 'en',
-    image: 'us.svg',
+    name: "English",
+    lang: "en",
+    image: "us.svg",
   },
   {
     id: 3n,
-    name: 'Spanish',
-    lang: 'es',
-    image: 'es.svg',
+    name: "Spanish",
+    lang: "es",
+    image: "es.svg",
+  },
+  {
+    id: 1n,
+    name: "Portuguese",
+    lang: "pt",
+    image: "br.svg",
   },
   {
     id: 4n,
-    name: 'French',
-    lang: 'fr',
-    image: 'fr.svg',
+    name: "French",
+    lang: "fr",
+    image: "fr.svg",
   },
   {
     id: 5n,
-    name: 'German',
-    lang: 'de',
-    image: 'de.svg',
+    name: "German",
+    lang: "de",
+    image: "de.svg",
   },
 ];
